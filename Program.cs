cf0e40d02676aa2b93fb5bf971380c681bb2a9be
VitorPaulon <71847592+vtrpaulon@users.noreply.github.com>
2023-12-25 15:00:06 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        //Declarando variaveis
+
+        string Nome = "Vitor";
+        char Sexo = 'M';
+        int Idade = 36;
+        float Altura = 1.76f;
+        bool Programador = true;
+        double SalarioProgramadorIniciante = 5000.00;
+
+        Console.WriteLine("Nome: " + Nome);
+        Console.WriteLine("Sexo: " + Sexo);
+        Console.WriteLine("Idade: " + Idade);
+        Console.WriteLine("Altura: " + Altura);
+        Console.WriteLine("Programador: " + Programador);
+        Console.WriteLine("Salario de programador iniciante: " + SalarioProgramadorIniciante);
+    }
+}
